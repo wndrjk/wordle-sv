07879fdeab3d69cf84a9254d090a74613f008c8f
wndrjk <155890415+wndrjk@users.noreply.github.com>
2025-07-21 16:28:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/wordle-sv/",
   plugins: [svelte({
     preprocess: vitePreprocess()
-  })],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: `[name]-v${version}.[ext]`,
-        entryFileNames: `[name]-v${version}.js`,
-        dir: "./dist",
-      }
-    }
-  }
+  })]
 });
